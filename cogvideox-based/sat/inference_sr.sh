8d7ce0c68a845e1348a4af0377c393ff09e32f67
CSRuiXie <165138837+CSRuiXie@users.noreply.github.com>
2025-01-06 14:54:58 +0800

    Update inference_sr.sh

--- cogvideox-based/sat/inference_sr.sh
+++ cogvideox-based/sat/inference_sr.sh
@@ -4,7 +4,7 @@ echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
 
 environs="WORLD_SIZE=1 RANK=0 LOCAL_RANK=0 LOCAL_WORLD_SIZE=1"
 
-run_cmd="$environs python sample_sr.py --base configs/cogvideox_5b_infer_sr.yaml"
+run_cmd="$environs python sample_sr.py --base configs/cogvideox_5b/cogvideox_5b_infer_sr.yaml"
 
 echo ${run_cmd}
 eval ${run_cmd}
